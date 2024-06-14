23c125f5be98e3c5c210c7f1cc2df97521fbb678
Zhou <141890618+RobertoZhou@users.noreply.github.com>
2024-06-14 17:14:03 -0300

    Repetições (Parte 1)

---
+++ Módulo E/aula13/ambiente.js
@@ -0,0 +1,24 @@
+contador = 1
+do {
+    console.log(`Bem-Vindo!`)
+    contador++
+} while(contador <= 6)
+
+
+/*
+var contador = 1
+while(contador <= 6){
+    console.log(`Passo ${contador}`)
+    contador++
+}
+*/
+
+
+/*
+console.log("Tudo-Bem?")
+console.log("Tudo-Bem?")
+console.log("Tudo-Bem?")
+console.log("Tudo-Bem?")
+console.log("Tudo-Bem?")
+console.log("Tudo-Bem?")
+*/
