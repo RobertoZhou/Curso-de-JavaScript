b2ac476ee5492f8af3b85fd8d298c3b5377b90ac
Zhou <141890618+RobertoZhou@users.noreply.github.com>
2024-06-14 17:52:57 -0300

    Repetições (Parte 2)

---
+++ Módulo E/aula14/ambiente.js
@@ -0,0 +1,9 @@
+var contador = 1
+while(contador <= 5) {
+    console.log(`Contador: ${contador}`)
+    contador++
+}
+
+for(var contador = 1; contador <= 5; contador++) {
+    console.log(`Contador: [${contador}]`)
+}
