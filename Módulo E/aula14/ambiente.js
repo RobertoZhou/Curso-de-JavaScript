f5177f8a24700c990e7b68ee5b10a35a2237619d
Zhou <141890618+RobertoZhou@users.noreply.github.com>
2024-06-19 00:05:03 -0300

    Módulo E

--- Módulo E/aula14/ambiente.js
+++
@@ -1,9 +0,0 @@
-var contador = 1
-while(contador <= 5) {
-    console.log(`Contador: ${contador}`)
-    contador++
-}
-
-for(var contador = 1; contador <= 5; contador++) {
-    console.log(`Contador: [${contador}]`)
-}
