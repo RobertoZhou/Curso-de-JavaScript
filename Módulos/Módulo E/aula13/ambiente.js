6a45ab63b063f1a1d3e3e9ad8183387a027f529a
Zhou <141890618+RobertoZhou@users.noreply.github.com>
2024-06-19 00:07:07 -0300

    Módulo E

---
+++ Módulos/Módulo E/aula13/ambiente.js
@@ -0,0 +1,25 @@
+// Executa o código primeiro, depois executa o while
+contador = 3
+do {
+    console.log(`Bem-Vindo!`)
+    contador++
+} while(contador == 6)
+
+
+/*
+var contador = 1
+while(contador <= 6){
+    console.log(`Passo ${contador}`)
+    contador++
+}
+*/
+
+
+/*
+console.log("Tudo-Bem?")
+console.log("Tudo-Bem?")
+console.log("Tudo-Bem?")
+console.log("Tudo-Bem?")
+console.log("Tudo-Bem?")
+console.log("Tudo-Bem?")
+*/
